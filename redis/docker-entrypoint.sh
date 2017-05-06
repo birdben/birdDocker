8b51418dbd00a787142b3772fd4ab94c6c7c8b0a
birdben <191654006@163.com>
2017-05-06 17:45:21 +0800

    修改redis镜像继承tools:v2基础镜像，并且使用docker-entrypoint.sh启动，并且支持root用户和redis用户启动容器

---
+++ redis/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# first arg is `-f` or `--some-option`
+# or first arg is `something.conf`
+# echo $@
+if [ "${1#-}" != "$1" ]; then
+	set -- redis-server "$@"
+fi
+
+# allow the container to be started with `--user`
+# TODO
+if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+	chown -R redis:redis /redis/data
+	chown -R redis:redis /redis/config
+	chown -R redis:redis /redis/logs
+	set -- gosu redis "$@"
+fi
+
+exec "$@"
---
+++ redis/run_root.sh
@@ -0,0 +1,1 @@
+docker run -itd -p 6379:6379 -v /Users/yunyu/workspace_git/birdDocker/redis/data:/redis/data -v /Users/yunyu/workspace_git/birdDocker/redis/config:/redis/config -v /Users/yunyu/workspace_git/birdDocker/redis/logs:/redis/logs --name redis_root birdben/redis:v2 redis-server /redis/config/redis.conf --protected-mode no
