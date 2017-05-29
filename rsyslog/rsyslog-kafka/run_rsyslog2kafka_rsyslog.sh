35ce2e49525745883b12ebebbac4a0c263ae0f1f
birdben <191654006@163.com>
2017-05-29 18:11:26 +0800

    优化docker启动配置，使用默认CMD启动参数，将run.sh脚本启动参数移至CMD默认启动参数，并且使用--link别名设置关联容器

--- rsyslog/rsyslog-kafka/run_rsyslog2kafka_rsyslog.sh
+++ rsyslog/rsyslog-kafka/run_rsyslog2kafka_rsyslog.sh
@@ -1,3 +1,3 @@
-docker run -itd --user rsyslog -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog-kafka/logfile:/usr/share/rsyslog/logfile -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog-kafka/config:/etc/rsyslog.d --link kafka --name rsyslog2kafka_rsyslog birdben/rsyslog-kafka:v2 rsyslogd -d -n -f /etc/rsyslog.d/rsyslog2kafka.conf -i /usr/share/rsyslog/pidfile/rsyslog2kafka.pid
+docker run -itd --user rsyslog -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog-kafka/logfile:/usr/share/rsyslog/logfile -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog-kafka/config:/etc/rsyslog.d --link kafka_kafka:kafka --name rsyslog2kafka_rsyslog birdben/rsyslog-kafka:v2
 # -d:开启rsyslog的DEBUG模式启动
 # -n:rsyslog前台启动
--- rsyslog/rsyslog/run_rsyslog2file_root.sh
+++ rsyslog/rsyslog/run_rsyslog2file_root.sh
@@ -1,3 +1,3 @@
-docker run -itd -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog/logfile:/usr/share/rsyslog/logfile -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog/config:/etc/rsyslog.d --name rsyslog2file_root birdben/rsyslog:v2 rsyslogd -d -n -f /etc/rsyslog.d/rsyslog2file.conf -i /usr/share/rsyslog/pidfile/rsyslog2file.pid
+docker run -itd -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog/logfile:/usr/share/rsyslog/logfile -v /Users/yunyu/workspace_git/birdDocker/rsyslog/rsyslog/config:/etc/rsyslog.d --name rsyslog2file_root birdben/rsyslog:v2
 # -d:开启rsyslog的DEBUG模式启动
 # -n:rsyslog前台启动
