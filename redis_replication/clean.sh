99e155c1e53de7cfb65943d104cb0392334d8c4a
birdben <191654006@163.com>
2017-07-29 18:42:03 +0800

    添加redis_replication主从环境

---
+++ redis_replication/clean.sh
@@ -0,0 +1,11 @@
+# 删除redis1数据和日志
+rm -rf ./volumes/redis1/data
+rm -rf ./volumes/redis1/logs
+
+# 删除redis2数据和日志
+rm -rf ./volumes/redis2/data
+rm -rf ./volumes/redis2/logs
+
+# 删除redis3数据和日志
+rm -rf ./volumes/redis3/data
+rm -rf ./volumes/redis3/logs
